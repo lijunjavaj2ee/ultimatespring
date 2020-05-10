7e9583f26d584d2d6eef627abfb380c50efe210f
lijunjavaj2ee <lijunjavaj2ee@163.com>
2020-05-10 20:24:06 +0800

    add some java files

---
+++ src/main/java/com/proto/ChessPieceUnit.java
@@ -0,0 +1,4 @@
+package com.proto;
+
+public class ChessPieceUnit {
+}
---
+++ src/main/java/com/upload/GtTest.java
@@ -0,0 +1,7 @@
+package com.upload;
+
+public class GtTest {
+    public static void main(String[] args) {
+        System.out.println("git update");
+    }
+}
